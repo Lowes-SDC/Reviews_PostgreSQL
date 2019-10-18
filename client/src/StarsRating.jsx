2e67f2b8920b9a1cae986ba3324a501608bd6216
Juan Galeazzi <jgaleazzi@gmail.com>
2019-10-18 17:20:33 -0500

    added styles

--- client/src/StarsRating.jsx
+++ client/src/StarsRating.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarHalfIcon from '@material-ui/icons/StarHalf';
 
+
+const StarsRatingStyle = {
+  width:'100%',
+  fontFamily:'Helvetica,Arial,sans-serif',
+  fontSize:'14px'
+}
 
 const StarsRating = (props) => {
 
-  let stars = Math.floor(props.stars)
-  console.log('stars' +stars)
-  return (
-    <div>
-      <div>
-       {props.totalVotes} ratings
-        <div>
+  let stars = Math.floor(props.stars);
+  let remainder = Number(props.stars%1);
+  let divs = [];
 
-          &#9733;
-          &#9733;
-          &#9733;
-          &#9733;
-          &#9733;
+  for (let i =0 ; i< stars; i++) {
+    divs.push(<StarIcon key={i}/>)
+  }
+  if (remainder > 0.2 && remainder < 0.8) {
+    divs.push(<StarHalfIcon key={6}/>)
+  }
+  if (remainder > 0.8) {
+    divs.push(<StarIcon key={6}/>)
+  }
+
+  return (
+    <div style={StarsRatingStyle}>
+      <div>
+       {props.totalVotes} Ratings
+        <div>
+          {divs}
         </div>
       {props.stars} average
       </div>
